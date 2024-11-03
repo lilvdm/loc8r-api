b96a5abe59dc469b3aba56d217f8cdf9047d44ff
lilvdm <lil.vdm97@gmail.com>
2024-11-03 22:15:58 +0900

    Add files via upload

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -39,23 +39,6 @@ const homelist = (req, res) => {
     });
 };
 
-
-    request(requestOptions, (err, { statusCode }, body) => {
-        let data = [];
-        if (err) {
-            console.error(err);
-            renderHomepage(req, res, data);
-            return;
-        }
-        if (statusCode === 200 && body.length) {
-            data = body.map(item => {
-                item.distance = formatDistance(item.distance);
-                return item;
-            });
-        }
-        renderHomepage(req, res, data);
-    });
-
     const formatDistance = (distance) => {
         let thisDistance = 0;
         let unit = 'm';
